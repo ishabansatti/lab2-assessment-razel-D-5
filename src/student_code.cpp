6c828d6c5e42c766c8d64e2d921c5a62ecf85985
Dawood Javed Malik <worldanimated8@gmail.com>
2026-02-18 15:16:19 +0500

    Correct Celsius calculation in fahrenheitToCelsius
    Fix the fahrenheitToCelsius function to correctly calculate Celsius from Fahrenheit.

--- src/student_code.cpp
+++ src/student_code.cpp
@@ -34,6 +34,7 @@ Formula:
     Celsius = (Fahrenheit - 32) * 5 / 9
 */
 float fahrenheitToCelsius(float fahrenheit) {
+    float celsius;
     Celsius = (fahrenheit - 32) * 5 / 9;
     //return celsius;
     return celsius;
